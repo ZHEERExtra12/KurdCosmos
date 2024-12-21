a7e3c7eeb0bee17bb188f239cade737ecba5520e
ZHEER <yassin.zheer10@gmail.com>
2024-12-21 19:55:12 +0300

    Add files via upload

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,28 +6,44 @@ import '../index.css'
 
 
 const Login = ({updateLogin}) => {
+    const [info, setInfo] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
         <>
 
-            <form>
+            
                 <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 font-nato backdrop-blur-sm">
                     <div className="bg-gray-950 p-6 rounded-lg shadow-lg max-w-sm w-80 relative">
                         <h2 className="text-2xl mb-2  text-white pt-0 pl-4  font-bold ">Kurd<span className="text-yellow-500">Cosmos</span></h2>
-                        <button onClick={()=>{updateLogin("")}} className="absolute  top-7 right-4"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#eab308"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg></button>
+                        <button onClick={()=>{updateLogin("")}} className="absolute  top-7 right-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#eab308"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg>
+                        </button>
+
                         <h2 className="text-right mt-9 mb-3 text-gray-400" >ئیمەیڵ</h2>
 
-                        <input type="email" className="w-full h-9 rounded-lg bg-gray-800 placeholder:text-right placeholder:mr-2 placeholder:text-xs text-right text-gray-400 text-sm" placeholder="ئیمەیڵ" />
+                        <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} className="w-full h-9 rounded-lg bg-gray-800 placeholder:text-right placeholder:mr-2 placeholder:text-xs text-right text-gray-400 text-sm" placeholder="ئیمەیڵ" />
 
                         <h2 className="text-right mt-9 mb-3 text-gray-400">ووشەیی نهێنی</h2>
-                        <input type="password" className="w-full h-9 rounded-lg bg-gray-800 placeholder:text-right placeholder:mr-2 placeholder:text-xs text-right text-gray-400 text-sm" placeholder=" ووشەیی نهێنی" />
+                        <input type="password" onChange={(e)=>setPassword(e.target.value)} value={password} className="w-full h-9 rounded-lg bg-gray-800 placeholder:text-right placeholder:mr-2 placeholder:text-xs text-right text-gray-400 text-sm" placeholder=" ووشەیی نهێنی" />
 
 
-                        <p className="text-white text-center mt-3 text-xs"><button className="text-yellow-500 hover:underline" onClick={()=>{updateLogin("reg")}} type="button">خۆتۆمارکردن </button>هەژمارت نیە ؟ </p>
+                        <p className="text-white text-center mt-3 text-xs">
+                            <button className="text-yellow-500 hover:underline" 
+                            onClick={()=>{updateLogin("reg")}} type="button">خۆتۆمارکردن </button>هەژمارت نیە ؟    
+                        </p>
 
-                        <button className="bg-yellow-500 text-white w-full py-4 mt-4 rounded-md" type="submit">چوونەژورەوە</button>
+                        <p className="text-center text-red-400 mt-3 text-sm">{info}</p>
+                        <button className="bg-yellow-500 text-white w-full py-4 mt-4 rounded-md" onClick={()=>{
+                        email == "" && password == "" ? setInfo("تکایە زانیاریەکانت بنووسە") : updateLogin("")
+                        //  if(email == "" && password == "") {
+                        //         setInfo("تکایە زانیاریەکانت بنووسە")
+                        //  }
+                        //  else { updateLogin("")}
+                        }}>چوونەژورەوە</button>
                     </div>
                 </div>
-            </form>
+            
 
 
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -23,8 +23,7 @@ const Register=({updateLogin})=>{
           
           return(
                     <>
-                              
-                              
+                              <form>
                               <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 font-nato backdrop-blur-sm">
                                         <div className="bg-gray-950 p-6 rounded-lg shadow-lg max-w-sm w-80 relative">
                                                   <h2 className="text-2xl mb-2  text-white pt-0 pl-4  font-bold ">Kurd<span className="text-yellow-500">Cosmos</span></h2>
@@ -32,19 +31,19 @@ const Register=({updateLogin})=>{
                                                   <button type="button" className="absolute  top-7 right-4 z-10" onClick={()=>{updateLogin("")}}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#eab308"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg></button>
 
                                                   <h2 className="text-right mt-6 mb-2 text-gray-400">ناو</h2>
-                                                  <input type="text" placeholder="ناوت" className="w-full h-9 rounded-lg bg-gray-800 placeholder:text-right placeholder:mr-2 placeholder:text-xs text-right text-gray-400 text-sm" />
+                                                  <input type="text" placeholder="ناوت" className="w-full h-9 rounded-lg bg-gray-800 placeholder:text-right placeholder:mr-2 placeholder:text-xs text-right text-gray-400 text-sm" required/>
 
                                                   <h2 className="text-right mt-6 mb-2 text-gray-400">بەروار</h2>
                                                   <input type="date" required className="w-full h-9 rounded-lg bg-gray-800 placeholder:text-right placeholder:mr-2 placeholder:text-xs text-right text-gray-400 text-sm" />
 
                                                   <h2 className="text-right mt-6 mb-2 text-gray-400">ئیمەیڵ</h2>
-                                                  <input type="text" className="w-full h-9 rounded-lg bg-gray-800 placeholder:text-right placeholder:mr-2 placeholder:text-xs text-right text-gray-400 text-sm" placeholder="ئیمەیڵ" required/>
+                                                  <input type="email" className="w-full h-9 rounded-lg bg-gray-800 placeholder:text-right placeholder:mr-2 placeholder:text-xs text-right text-gray-400 text-sm" placeholder="ئیمەیڵ" required/>
 
                                                   <h2 className="text-right mt-6 mb-2 text-gray-400">ووشەیی نهێنی</h2>
-                                                  <input type="password" onChange={(e)=>setPass1(e.target.value)} value={pass1} className="w-full h-9 rounded-lg bg-gray-800 placeholder:text-right placeholder:mr-2 placeholder:text-xs text-right text-gray-400 text-sm" placeholder="  نابێ لە ٨ پیت و هێما کەمتر بێ  " />
+                                                  <input type="password" onChange={(e)=>setPass1(e.target.value)} value={pass1} className="w-full h-9 rounded-lg bg-gray-800 placeholder:text-right placeholder:mr-2 placeholder:text-xs text-right text-gray-400 text-sm" placeholder="  نابێ لە ٨ پیت و هێما کەمتر بێ  " required/>
 
                                                   <h2 className="text-right mt-6 mb-2 text-gray-400" id="pass"> دووبارە ووشەیی نهێنی</h2>
-                                                  <input type="password" onChange={(e)=>setPass2(e.target.value)} value={pass2} className="w-full h-9 rounded-lg bg-gray-800 placeholder:text-right placeholder:mr-2 placeholder:text-xs text-right text-gray-400 text-sm" placeholder=" دوبارە وشەیی نهێنی" />
+                                                  <input type="password" onChange={(e)=>setPass2(e.target.value)} value={pass2} className="w-full h-9 rounded-lg bg-gray-800 placeholder:text-right placeholder:mr-2 placeholder:text-xs text-right text-gray-400 text-sm" placeholder=" دوبارە وشەیی نهێنی" required/>
                                                   {/*we use onchange to allow the value to be change
                                                   and we use the value to 
                                                   */}
@@ -60,12 +59,13 @@ const Register=({updateLogin})=>{
                                                   <p className="text-white text-center mt-3 text-xs"><button className="text-yellow-500 hover:underline" onClick={()=>{updateLogin("login")}} type="button">چوونەژورەوە </button>هەژمارت هەیە  ؟ </p>
                                                   
                                                  
-                                                  <button className="bg-yellow-500 text-white w-full py-4 mt-4 rounded-md" type="button" onClick={()=>{pass1!=pass2 ? setError("پاسوردەکەوەکویەک نیە"): null}}>تۆمارکردن</button>
+                                                  <button className="bg-yellow-500 text-white w-full py-4 mt-4 rounded-md" type="button" onClick={()=>{ pass1!=pass2 ? setError("پاسوردەکەوەکویەک نیە") : ( pass1 = pass2 ? updateLogin("") : setError("زانیاریەکانت بنووسە")) }}>تۆمارکردن</button>
                                                   
                                                  
                                                   
                                         </div>
                               </div>
+                        </form>
                               
                     </>
           )
